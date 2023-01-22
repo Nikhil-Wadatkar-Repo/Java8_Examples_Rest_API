19f555fdd50eee5faf2c885843c18d6a572535e9
Nikhil4693 <nikhil.wadatkar4693@gmail.com>
2023-01-23 00:54:32 +0530

    Project Added

---
+++ src/main/java/com/nt/Java8RestAPIs/Java8RestApIsApplication.java
@@ -0,0 +1,13 @@
+package com.nt.Java8RestAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8RestApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8RestApIsApplication.class, args);
+	}
+
+}
